c0090d545890111c8ce8e0a4db0d28b51a7a4de2
victorernesto <88003634+victorernesto@users.noreply.github.com>
2022-05-30 22:07:13 -0600

    INICIO

---
+++ menuYinicio/menuYinicio/Controllers/InicioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace menuYinicio.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+     
+        public ActionResult inicio()
+        {
+            return View();
+        }
+
+        public ActionResult nosotros()
+        {
+            return View();
+        }
+
+        public ActionResult menu()
+        {
+            return View();
+        }
+
+       
+    }
+}
